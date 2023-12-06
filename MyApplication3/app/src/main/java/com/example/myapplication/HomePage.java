d2f270736c8d2abf6987b4741025a3f50bbd5392
ConfettiRaindrop <camilafigueira@outlook.com>
2023-12-05 17:44:00 -0800

    fix file name add page to nav

--- MyApplication3/app/src/main/java/com/example/myapplication/HomePage.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/HomePage.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Teleop#newInstance} factory method to
+ * Use the {@link HomePage#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Teleop extends Fragment {
+public class HomePage extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Teleop extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Teleop() {
+    public HomePage() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class Teleop extends Fragment {
      * @return A new instance of fragment Teleop.
      */
     // TODO: Rename and change types and number of parameters
-    public static Teleop newInstance(String param1, String param2) {
-        Teleop fragment = new Teleop();
+    public static HomePage newInstance(String param1, String param2) {
+        HomePage fragment = new HomePage();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Teleop extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teleop, container, false);
+        return inflater.inflate(R.layout.fragment_mainpage, container, false);
     }
 }

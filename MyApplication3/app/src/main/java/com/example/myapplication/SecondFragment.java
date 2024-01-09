04b8c955d0335cf5d05390b4eb38c2a4d56d13ea
DUO ZHONG <s-jzhong@lwsd.org>
2024-01-08 19:14:06 -0800

    80% done

--- MyApplication3/app/src/main/java/com/example/myapplication/SecondFragment.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -2,8 +2,11 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +20,7 @@ import com.example.myapplication.databinding.FragmentSecondBinding;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+    public int amp = 0;
 
     @Override
     public View onCreateView(
@@ -33,7 +37,7 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        binding.next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -47,10 +51,48 @@ public class SecondFragment extends Fragment {
         float width = displayMetrics.widthPixels;
         ViewGroup.LayoutParams layoutParams = binding.relativeLayoutFirst.getLayoutParams();
         layoutParams.width = (int) width;
-        layoutParams.height = (int) height;
+        layoutParams.height = (int) height + 380;
         binding.relativeLayoutFirst.setLayoutParams(layoutParams);
-        binding.buttonSecond.setTranslationX((width - 300)/ 2);
-        binding.textView.setTranslationX((width - 360)/ 2);
+        binding.next.setTranslationX((width - 300)/ 2.0f);
+        binding.next.setTranslationY(height + 230);
+        binding.title.setTranslationX((width - 136)/ 2.0f);
+        binding.broke.setTranslationX(width * 0.073f);
+        binding.broke.setTranslationY(height * 0.216f);
+        binding.defense.setTranslationX(width * 0.073f);
+        binding.defense.setTranslationY(height * 0.288f);
+        binding.ground.setTranslationX(width * 0.073f);
+        binding.ground.setTranslationY(height * 0.360f);
+        binding.source.setTranslationX(width * 0.073f);
+        binding.source.setTranslationY(height * 0.432f);
+        binding.ampNotes.setTranslationX(width * 0.073f);
+        binding.ampNotes.setTranslationY(height * 0.504f);
+        binding.minusAmp.setTranslationX(width * 0.366f);
+        binding.minusAmp.setTranslationY(height * 0.453f);
+        binding.minusAmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (amp > 0){
+                    amp --;
+                    binding.ampCounter.setText("" + amp);
+                }
+            }
+        });
+        binding.plusAmp.setTranslationX(width * 0.732f);
+        binding.plusAmp.setTranslationY(height * 0.453f);
+        binding.plusAmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                amp ++;
+                binding.ampCounter.setText("" + amp);
+            }
+        });
+        binding.ampCounter.setTranslationX(width * 0.598f);
+        binding.ampCounter.setTranslationY(height * 0.525f);
+        binding.speakerNotesUnamp.setTranslationX(width * 0.073f);
+        binding.speakerNotesUnamp.setTranslationY(height * 0.619f);
+
+        binding.speakerNotesAmp.setTranslationX(width * 0.073f);
+        binding.speakerNotesAmp.setTranslationY(height * 0.835f);
     }
 
     @Override

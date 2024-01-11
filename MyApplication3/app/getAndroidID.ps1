6bfe56fccd02919f7861bf242eb7fb14039cf007
atian7064 <56783429+atian7064@users.noreply.github.com>
2024-01-10 19:53:06 -0800

    Replaced hardcoded paths with dyanmic

--- MyApplication3/app/getAndroidID.ps1
+++ MyApplication3/app/getAndroidID.ps1
@@ -1,4 +1,4 @@
-cd C:\Users\camil\AppData\Local\Android\Sdk\platform-tools
+cd $env:LOCALAPPDATA\Android\Sdk\platform-tools
 $androidId = ./adb devices | Select-Object -Skip 1 | Select-Object -SkipLast 1 | ForEach-Object { $_.Substring(0, $_.Length-7) }
 $androidIdArray = $androidId.Split("\n")
 For ($i=0; $i -lt $androidIdArray.Length; $i++){

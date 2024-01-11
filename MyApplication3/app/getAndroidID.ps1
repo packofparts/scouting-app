35627019e521b17d9a147dd1adc961d0677da451
atian7064 <56783429+atian7064@users.noreply.github.com>
2024-01-10 20:48:03 -0800

    Added file dection

--- MyApplication3/app/getAndroidID.ps1
+++ MyApplication3/app/getAndroidID.ps1
@@ -2,6 +2,13 @@ cd $env:LOCALAPPDATA\Android\Sdk\platform-tools
 $androidId = ./adb devices | Select-Object -Skip 1 | Select-Object -SkipLast 1 | ForEach-Object { $_.Substring(0, $_.Length-7) }
 $androidIdArray = $androidId.Split("\n")
 For ($i=0; $i -lt $androidIdArray.Length; $i++){
-./adb -s $androidIdArray[$i] backup -all
+    $checkPathExists = (./adb -s $androidIdArray[$i] shell ls sdcard/download/readyToTransfer.txt) | Out-String
+    if($checkPathExists -match 'sdcard/download/readyToTransfer.txt'){
+        echo "yay"
+    }
+    else{
+        echo "bruh"
+    }
+    Start-Sleep -Seconds 1
 }
-Start-Sleep -Seconds 5
+

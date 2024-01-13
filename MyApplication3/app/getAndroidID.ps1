3e78f57eee8e2eaf84b14ce75a2cb8f2f2673559
atian7064 <56783429+atian7064@users.noreply.github.com>
2024-01-13 15:57:56 -0800

    Continously run pull command

--- MyApplication3/app/getAndroidID.ps1
+++ MyApplication3/app/getAndroidID.ps1
@@ -1,13 +1,10 @@
-cd $env:LOCALAPPDATA\Android\Sdk\platform-tools
+Set-Location $env:LOCALAPPDATA\Android\Sdk\platform-tools
+While($true){
 $androidId = ./adb devices | Select-Object -Skip 1 | Select-Object -SkipLast 1 | ForEach-Object { $_.Substring(0, $_.Length-7) }
 $androidIdArray = $androidId.Split("\n")
 For ($i=0; $i -lt $androidIdArray.Length; $i++){
-    $checkPathExists = (./adb -s $androidIdArray[$i] shell ls sdcard/download/readyToTransfer.txt) | Out-String
-    if($checkPathExists -match 'sdcard/download/readyToTransfer.txt'){
-        echo "yay"
-    }
-    else{
-        echo "bruh"
-    }
+    ./adb -s $androidIdArray[$i] pull "sdcard/documents" "C:\Users\user\Downloads"
 }
+}
+
 

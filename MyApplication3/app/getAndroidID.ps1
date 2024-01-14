59569cb10929b392d1a2c5494f35590516afc96f
atian7064 <56783429+atian7064@users.noreply.github.com>
2024-01-13 16:18:25 -0800

    Added check for no devices connected

--- MyApplication3/app/getAndroidID.ps1
+++ MyApplication3/app/getAndroidID.ps1
@@ -1,5 +1,8 @@
 Set-Location $env:LOCALAPPDATA\Android\Sdk\platform-tools
 While($true){
+if (-Not (./adb devices | Select-Object -Skip 1)){
+    continue
+}
 $androidId = ./adb devices | Select-Object -Skip 1 | Select-Object -SkipLast 1 | ForEach-Object { $_.Substring(0, $_.Length-7) }
 $androidIdArray = $androidId.Split("\n")
 For ($i=0; $i -lt $androidIdArray.Length; $i++){

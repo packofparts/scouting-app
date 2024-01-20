21e86274c5c7221268406eea98c29fa0be044a42
atian7064 <56783429+atian7064@users.noreply.github.com>
2024-01-20 15:05:54 -0800

    Fixed mkdir command not working on all deivces

--- MyApplication3/app/getAndroidID.ps1
+++ MyApplication3/app/getAndroidID.ps1
@@ -1,13 +1,12 @@
 Set-Location $env:LOCALAPPDATA\Android\Sdk\platform-tools
 While($true){
-    ./adb shell mkdir /sdcard/ScoutingData
 if (-Not (./adb devices | Select-Object -Skip 1)){
     continue
 }
 $androidId = ./adb devices | Select-Object -Skip 1 | Select-Object -SkipLast 1 | ForEach-Object { $_.Substring(0, $_.Length-7) }
 $androidIdArray = $androidId.Split("\n")
 For ($i=0; $i -lt $androidIdArray.Length; $i++){
-    
+    ./adb -s $androidIdArray[$i] shell mkdir /sdcard/ScoutingData
 }
 }
 

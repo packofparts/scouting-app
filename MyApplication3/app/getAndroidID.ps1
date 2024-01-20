95e9b683564e5c555f0a20c2778091749a179b88
atian7064 <56783429+atian7064@users.noreply.github.com>
2024-01-20 15:01:48 -0800

    Create ScoutingData folder

--- MyApplication3/app/getAndroidID.ps1
+++ MyApplication3/app/getAndroidID.ps1
@@ -1,13 +1,13 @@
 Set-Location $env:LOCALAPPDATA\Android\Sdk\platform-tools
 While($true){
+    ./adb shell mkdir /sdcard/ScoutingData
 if (-Not (./adb devices | Select-Object -Skip 1)){
     continue
 }
 $androidId = ./adb devices | Select-Object -Skip 1 | Select-Object -SkipLast 1 | ForEach-Object { $_.Substring(0, $_.Length-7) }
 $androidIdArray = $androidId.Split("\n")
 For ($i=0; $i -lt $androidIdArray.Length; $i++){
-    ./adb -s $androidIdArray[$i] pull "sdcard/documents" "C:\Users\user\Downloads"
+    
 }
 }
-
 

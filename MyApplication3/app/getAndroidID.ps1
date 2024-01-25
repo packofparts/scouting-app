233ecc94f49b8605dd93d1a82089eb96e09d4c16
atian7064 <56783429+atian7064@users.noreply.github.com>
2024-01-24 19:36:55 -0800

    Removed redundent continue

--- MyApplication3/app/getAndroidID.ps1
+++ MyApplication3/app/getAndroidID.ps1
@@ -7,11 +7,11 @@ $androidId = ./adb devices | Select-Object -Skip 1 | Select-Object -SkipLast 1 |
 $androidIdArray = $androidId.Split("\n")
 For ($i=0; $i -lt $androidIdArray.Length; $i++){
     ./adb -s $androidIdArray[$i] shell mkdir /sdcard/ScoutingData
-    if (./adb shell "(ls /sdcard/ScoutingData/test.txt >> /dev/null 2>&1 && echo y)") {
-        continue
+    if (-Not (./adb shell "(ls /sdcard/ScoutingData/lock.txt >> /dev/null 2>&1 && echo y)")) {
+        ./adb -s $androidIdArray[$i] pull /sdcard/ScoutingData $env:USERPROFILE\documents
+        ./adb -s $androidIdArray[$i] shell touch /sdcard/ScoutingData/lock.txt
     }
-    ./adb -s $androidIdArray[$i] pull /sdcard/ScoutingData $env:USERPROFILE\documents
-    ./adb -s $androidIdArray[$i] shell touch /sdcard/ScoutingData/test.txt
+    
 }
 }
 

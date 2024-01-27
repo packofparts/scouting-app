9597ad71ce60dee68c0d5e724752cbc6e633ad6d
Martin Taillefer <geeknoid@users.noreply.github.com>
2024-01-26 19:02:24 -0800

    Enable CI integration with SonarCloud (#27)
    Co-authored-by: Martin Taillefer <mataille@microsoft.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.2" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "FRC-1294_scouting-app")
+    property("sonar.organization", "frc-1294")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }

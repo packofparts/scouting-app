ab662a2e8df5f5117be621441facb454ef6f14cb
evan singleton <esingleton08@gmail.com>
2024-02-03 16:05:28 -0800

    add light dark mode

--- app/src/main/java/com/example/myapplication/ThirdFragment.java
+++ app/src/main/java/com/example/myapplication/ThirdFragment.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import android.animation.ObjectAnimator;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 
@@ -18,12 +21,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
 
 import com.example.myapplication.databinding.FragmentThirdBinding;
-
+import com.google.android.material.textfield.TextInputLayout;
 
 
 /**
@@ -36,6 +43,7 @@ public class ThirdFragment extends Fragment {
 
 
     private FragmentThirdBinding binding;
+    ViewGroup v = null;
 
 
     @Override
@@ -43,6 +51,33 @@ public class ThirdFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState
     ){
         binding = FragmentThirdBinding.inflate(inflater, container, false);
+        v = container;
+        ColorStateList purple = ColorStateList.valueOf(Color.parseColor(("#6750A3")));
+        ColorStateList blue = ColorStateList.valueOf((Color.parseColor("#73C2F0")));
+        if (binding.switch1.isChecked()){
+            binding.switch1.setThumbTintList(purple);
+            binding.switch1.setTrackTintList(purple);
+        } else {
+            binding.switch1.setThumbTintList(blue);
+            binding.switch1.setTrackTintList(blue);
+        }
+        binding.switch2.setChecked(MainActivity.defense);
+        if (binding.switch2.isChecked()){
+            binding.switch2.setThumbTintList(blue);
+            binding.switch2.setTrackTintList(blue);
+        } else {
+            binding.switch2.setThumbTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+            binding.switch2.setTrackTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+        }
+        binding.switch3.setChecked(MainActivity.ground);
+        if (binding.switch3.isChecked()){
+            binding.switch3.setThumbTintList(blue);
+            binding.switch3.setTrackTintList(blue);
+        } else {
+            binding.switch3.setThumbTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+            binding.switch3.setTrackTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+        }
+
         return binding.getRoot();
     }
 
@@ -55,6 +90,55 @@ public class ThirdFragment extends Fragment {
                         .navigate(R.id.action_ThirdFragment_to_thirdFragment2);
             }
         });
+        ObjectAnimator animation = ObjectAnimator.ofFloat(binding.pop, "rotationX", new float[]{0f, 90f, 180f, 270f, 360f, 90f, 180f, 270f, 360f, 90f, 180f, 270f, 360f});
+        animation.setDuration(1000);
+        binding.pop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animation.start();
+                MainActivity.darkMode = !MainActivity.darkMode;
+                lightDark(v, MainActivity.darkMode);
+            }
+        });
+        binding.switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)  {
+                if (binding.switch1.isChecked()){
+                    binding.switch1.setThumbTintList(ColorStateList.valueOf(Color.parseColor("#6750A3")));
+                    binding.switch1.setTrackTintList(ColorStateList.valueOf(Color.parseColor("#6750A3")));
+                } else {
+                    binding.switch1.setThumbTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+                    binding.switch1.setTrackTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+                }
+                MainActivity.broke = binding.switch1.isChecked();
+            }
+        });
+        binding.switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)  {
+                if (binding.switch2.isChecked()){
+                    binding.switch2.setThumbTintList(ColorStateList.valueOf(Color.parseColor("#6750A3")));
+                    binding.switch2.setTrackTintList(ColorStateList.valueOf(Color.parseColor("#6750A3")));
+                } else {
+                    binding.switch2.setThumbTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+                    binding.switch2.setTrackTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+                }
+                MainActivity.broke = binding.switch2.isChecked();
+            }
+        });
+        binding.switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)  {
+                if (binding.switch3.isChecked()){
+                    binding.switch3.setThumbTintList(ColorStateList.valueOf(Color.parseColor("#6750A3")));
+                    binding.switch3.setTrackTintList(ColorStateList.valueOf(Color.parseColor("#6750A3")));
+                } else {
+                    binding.switch3.setThumbTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+                    binding.switch3.setTrackTintList(ColorStateList.valueOf(Color.parseColor("#73C2F0")));
+                }
+                MainActivity.broke = binding.switch3.isChecked();
+            }
+        });
         String[] typeContact = {"No contact", "Teammate to Teammate", "Teammate to opponent"};
         ArrayAdapter<String> contact = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, typeContact);
         binding.spinner.setAdapter(contact);
@@ -62,9 +146,45 @@ public class ThirdFragment extends Fragment {
 
         Editable numNotes = binding.numNotes.getText();
 
+        lightDark(v, MainActivity.darkMode);
 
+        Switch switch1 = view.findViewById(R.id.switch1);
+        switch1.isChecked();
+
+        Switch switch2 = view.findViewById(R.id.switch2);
+        switch2.isChecked();
+
+        Switch switch3 = view.findViewById(R.id.switch3);
+        switch3.isChecked();
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+    public void lightDark (ViewGroup v, boolean mode){
+        int bgColor = (mode ? (Color.BLACK) : Color.WHITE);
+        int textColor = (mode ? (Color.WHITE) : (Color.BLACK));
+        for (int i = 0; i < v.getChildCount(); i ++){
+            View child = v.getChildAt(i);
+            if (!(child instanceof Button)) {
+                child.setBackgroundColor(bgColor);
+                if (child instanceof TextView) {
+                    TextView tx = (TextView) child;
+                    tx.setTextColor(textColor);
+                }
+            }
+            if (child instanceof Switch) {
+                Switch tx = (Switch) child;
+                tx.setTextColor(textColor);
+            }
+            if (child instanceof ViewGroup){
+                lightDark((ViewGroup)child, mode);
+            }
         }
-
+    }
 
 
     //TODO: Rename parameter arguments, choose names that match
--- app/src/main/java/com/example/myapplication/ThirdFragment2.java
+++ app/src/main/java/com/example/myapplication/ThirdFragment2.java
@@ -1,11 +1,16 @@
 package com.example.myapplication;
 
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -23,6 +28,8 @@ public class ThirdFragment2 extends Fragment{
 
     private FragmentThird2Binding binding;
 
+    ViewGroup v = null;
+
     @Override
     public View onCreateView(
 
@@ -31,6 +38,7 @@ public class ThirdFragment2 extends Fragment{
 
     ){
         binding = FragmentThird2Binding.inflate(inflater, container, false);
+        v = container;
         return binding.getRoot();
     }
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -45,13 +53,44 @@ public class ThirdFragment2 extends Fragment{
                         .navigate(R.id.action_thirdFragment2_to_SecondFragment);
             }
         });
+        ObjectAnimator animation = ObjectAnimator.ofFloat(binding.pop, "rotationX", new float[]{0f, 90f, 180f, 270f, 360f, 90f, 180f, 270f, 360f, 90f, 180f, 270f, 360f});
+        animation.setDuration(1000);
+        binding.pop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animation.start();
+                MainActivity.darkMode = !MainActivity.darkMode;
+                lightDark(v, MainActivity.darkMode);
+            }
+        });
 
+        lightDark(v, MainActivity.darkMode);
 
 
 
 
 
     }
-
+    public void lightDark (ViewGroup v, boolean mode){
+        int bgColor = (mode ? (Color.BLACK) : Color.WHITE);
+        int textColor = (mode ? (Color.WHITE) : (Color.BLACK));
+        for (int i = 0; i < v.getChildCount(); i ++){
+            View child = v.getChildAt(i);
+            if (!(child instanceof Button)) {
+                child.setBackgroundColor(bgColor);
+                if (child instanceof TextView) {
+                    TextView tx = (TextView) child;
+                    tx.setTextColor(textColor);
+                }
+            }
+            if (child instanceof Switch) {
+                Switch tx = (Switch) child;
+                tx.setTextColor(textColor);
+            }
+            if (child instanceof ViewGroup){
+                lightDark((ViewGroup)child, mode);
+            }
+        }
+    }
     }
 

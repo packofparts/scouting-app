8ca3217b912282a866139d08899a269615cc111e
ConfettiRaindrop <camilafigueira@outlook.com>
2024-04-25 21:35:37 -0700

    checking in

---
+++ app/src/main/java/com/example/myapplication/csv_to_json.py
@@ -0,0 +1,35 @@
+import csv, json, datetime
+
+# Specify the directory path
+csv_file_name = r'C:\Users\camil\Robotics\JoinedData\PNCMP2024-MatchExport-Q100.csv' #TODO:check if file name need changing later
+json_file_name = r'C:\Users\camil\Robotics\JoinedData\DCMPQualMatches.json'
+
+all_matches = []
+total_time = None
+current_time = datetime.datetime.hour*1000 + datetime.datetime.minute*100 + datetime.datetime.second + float(1/datetime.datetime.microsecond)
+
+with open(csv_file_name, newline='', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    first_row = True
+    for row in csv_reader:
+        if first_row:
+            column_names = []
+            for column_name in row:
+                column_names.append(column_name.strip('\uFEFF'))
+            first_row = False
+        else:
+            current_record = {}
+            for i in range(len(column_names)):
+                current_record[column_names[i]] = row[i]
+            all_matches.append(current_record)
+    total_time = datetime.datetime.hour*1000 + datetime.datetime.minute*100 + datetime.datetime.second + float(1/datetime.datetime.microsecond) - current_time
+
+
+#print(all_matches)
+
+with open(json_file_name, 'w', encoding='utf-8') as json_file:
+    json_file.write(json.dumps(all_matches, indent=4))
+    total_time = datetime.datetime.hour*1000 + datetime.datetime.minute*100 + datetime.datetime.second + float(1/datetime.datetime.microsecond) - current_time
+    
+if total_time is not None:
+    print(total_time)
---
+++ app/src/main/java/com/example/myapplication/file_joiner.py
@@ -0,0 +1,31 @@
+import os
+
+# Specify the directory path
+directory_path = r'C:\Users\camil\Documents\ScoutingData'
+
+# Get a list of all ".txt" files in the directory
+json_files = [file for file in os.listdir(directory_path) if file.endswith('.json')]
+
+# Initialize an empty array
+merged_content = '['
+
+# Separator starts empty for first file
+separator = ''
+
+# Read the contents of each ".txt" file and append to the merged content
+for json_file in json_files:
+    file_path = os.path.join(directory_path, json_file)
+    with open(file_path, 'r', encoding='utf-8') as in_file:
+        merged_content += separator
+        separator = ','
+        merged_content += in_file.read()
+
+# Close the array
+merged_content += ']'
+
+# Write the merged content to the output file
+output_file_path = r'C:\Users\camil\Robotics\JoinedData\MergedData.json'
+with open(output_file_path, 'w', encoding='utf-8') as outfile:
+    outfile.write(merged_content)
+
+print(f"Merged data saved to {output_file_path}")

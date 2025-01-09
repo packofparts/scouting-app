e6c3f587468bebfec92961db502b6a2bc4de8209
MthreeJohnnyK <138945247+MthreeJohnnyK@users.noreply.github.com>
2025-01-08 19:36:36 -0800

    I added MatchData fields. (#129)

--- app/src/main/java/com/example/myapplication/MatchData.java
+++ app/src/main/java/com/example/myapplication/MatchData.java
@@ -8,14 +8,22 @@ import java.io.IOException;
 public class MatchData {
 
     //Pre-Game
-    String teamNumber = "";
-    String matchNumber = "";
+    private String teamNumber = "";
+    private String matchNumber = "";
 
-    
+    private boolean moveOutOfZone, inZone = false;
 
-    double analyzerScore = 0.0;
+    private int autoL1, autoL2, autoL3, autoL4, autoMissedCoral, teleOpL1, teleOpL2, teleOpL3, teleOpL4, teleopMissedCoral = 0;
 
-    String notes = "";
+    private int autoNet, autoProcessor, autoMissedAlgae, teleOpNet, teleOpProcessor, teleOpMissedAlgae = 0;
+
+    private double defPercent, defEffectiveness, brokePercent, stars = 0.0;
+
+    private int depth, climb = 0;
+
+    private double analyzerScore = 0.0;
+
+    private String notes = "";
 
 
 
@@ -51,4 +59,196 @@ public class MatchData {
         return analyzerScore;
     }
     public void setAnalyzerScore(double analyzerScore) {this.analyzerScore = analyzerScore;}
+
+    public boolean getMoveOutOfZone() {
+        return moveOutOfZone;
+    }
+
+    public void setMoveOutOfZone(boolean moveOutOfZone) {
+        this.moveOutOfZone = moveOutOfZone;
+    }
+
+    public int getAutoL1() {
+        return autoL1;
+    }
+
+    public void setAutoL1(int autoL1) {
+        this.autoL1 = autoL1;
+    }
+
+    public int getAutoL2() {
+        return autoL2;
+    }
+
+    public void setAutoL2(int autoL2) {
+        this.autoL2 = autoL2;
+    }
+
+    public int getAutoL3() {
+        return autoL3;
+    }
+
+    public void setAutoL3(int autoL3) {
+        this.autoL3 = autoL3;
+    }
+
+    public int getAutoL4() {
+        return autoL4;
+    }
+
+    public void setAutoL4(int autoL4) {
+        this.autoL4 = autoL4;
+    }
+
+    public int getAutoMissedCoral() {
+        return autoMissedCoral;
+    }
+
+    public void setAutoMissedCoral(int autoMissedCoral) {
+        this.autoMissedCoral = autoMissedCoral;
+    }
+
+    public int getTeleOpL1() {
+        return teleOpL1;
+    }
+
+    public void setTeleOpL1(int teleOpL1) {
+        this.teleOpL1 = teleOpL1;
+    }
+
+    public int getTeleOpL2() {
+        return teleOpL2;
+    }
+
+    public void setTeleOpL2(int teleOpL2) {
+        this.teleOpL2 = teleOpL2;
+    }
+
+    public int getTeleOpL3() {
+        return teleOpL3;
+    }
+
+    public void setTeleOpL3(int teleOpL3) {
+        this.teleOpL3 = teleOpL3;
+    }
+
+    public int getTeleOpL4() {
+        return teleOpL4;
+    }
+
+    public void setTeleOpL4(int teleOpL4) {
+        this.teleOpL4 = teleOpL4;
+    }
+
+    public int getTeleopMissedCoral() {
+        return teleopMissedCoral;
+    }
+
+    public void setTeleopMissedCoral(int teleopMissedCoral) {
+        this.teleopMissedCoral = teleopMissedCoral;
+    }
+
+    public int getAutoNet() {
+        return autoNet;
+    }
+
+    public void setAutoNet(int autoNet) {
+        this.autoNet = autoNet;
+    }
+
+    public int getAutoProcessor() {
+        return autoProcessor;
+    }
+
+    public void setAutoProcessor(int autoProcessor) {
+        this.autoProcessor = autoProcessor;
+    }
+
+    public int getAutoMissedAlgae() {
+        return autoMissedAlgae;
+    }
+
+    public void setAutoMissedAlgae(int autoMissedAlgae) {
+        this.autoMissedAlgae = autoMissedAlgae;
+    }
+
+    public int getTeleOpNet() {
+        return teleOpNet;
+    }
+
+    public void setTeleOpNet(int teleOpNet) {
+        this.teleOpNet = teleOpNet;
+    }
+
+    public int getTeleOpProcessor() {
+        return teleOpProcessor;
+    }
+
+    public void setTeleOpProcessor(int teleOpProcessor) {
+        this.teleOpProcessor = teleOpProcessor;
+    }
+
+    public int getTeleOpMissedAlgae() {
+        return teleOpMissedAlgae;
+    }
+
+    public void setTeleOpMissedAlgae(int teleOpMissedAlgae) {
+        this.teleOpMissedAlgae = teleOpMissedAlgae;
+    }
+
+    public double getDefPercent() {
+        return defPercent;
+    }
+
+    public void setDefPercent(double defPercent) {
+        this.defPercent = defPercent;
+    }
+
+    public double getDefEffectiveness() {
+        return defEffectiveness;
+    }
+
+    public void setDefEffectiveness(double defEffectiveness) {
+        this.defEffectiveness = defEffectiveness;
+    }
+
+    public double getBrokePercent() {
+        return brokePercent;
+    }
+
+    public void setBrokePercent(double brokePercent) {
+        this.brokePercent = brokePercent;
+    }
+
+    public double getStars() {
+        return stars;
+    }
+
+    public void setStars(double stars) {
+        this.stars = stars;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getClimb() {
+        return climb;
+    }
+
+    public void setClimb(int climb) {
+        this.climb = climb;
+    }
+
+    public boolean getInZone() {
+        return inZone;
+    }
+
+    public void setInZone(boolean inZone) {
+        this.inZone = inZone;
+    }
 }

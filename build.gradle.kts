f7c58069d8d813311faf239d788867b2dda9fd2a
Martin Taillefer <mataille@microsoft.com>
2025-01-18 10:26:37 -0800

    Update Sonar config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    id("org.sonarqube") version "4.4.1.3373"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 sonar {
   properties {
-    property("sonar.projectKey", "FRC-1294_scouting-app")
+    property("sonar.projectKey", "scouting-app")
     property("sonar.organization", "packofparts")
     property("sonar.host.url", "https://sonarcloud.io")
   }

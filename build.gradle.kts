22a3055cc49e153c771e6b8c533b09b203874d3e
Martin Taillefer <geeknoid@users.noreply.github.com>
2025-01-25 06:47:14 -0800

    Upgrade automation and versions (#138)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.3" apply false
+    alias(libs.plugins.android.application) apply false
     id("org.sonarqube") version "6.0.1.5171"
 }
 
 sonar {
   properties {
-    property("sonar.projectKey", "scouting-app")
+    property("sonar.projectKey", "packofparts_scouting-app")
     property("sonar.organization", "packofparts")
     property("sonar.host.url", "https://sonarcloud.io")
   }

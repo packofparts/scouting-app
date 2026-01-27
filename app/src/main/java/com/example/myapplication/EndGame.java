e0fa7c099749d1268b030f939fb9d52b3a55726a
Changhyung Lee <makerchlee@outlook.kr>
2026-01-26 22:47:14 -0800

    endgame fixes, changed to radio buttons

--- app/src/main/java/com/example/myapplication/EndGame.java
+++ app/src/main/java/com/example/myapplication/EndGame.java
@@ -12,7 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.RadioButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,10 +24,7 @@ public class EndGame extends Fragment {
     private EndGameBinding binding;
     ViewGroup v = null;
 
-    // Climb level selection
-
-    // Star rating
-    private int currentRating = 4; // Default 4 stars
+    private int currentRating = 4;
     private ImageView[] stars;
 
     @SuppressLint("SetTextI18n")
@@ -40,7 +37,6 @@ public class EndGame extends Fragment {
         binding = EndGameBinding.inflate(inflater, container, false);
         v = container;
 
-        // Initialize star array
         stars = new ImageView[]{
                 binding.star1,
                 binding.star2,
@@ -49,6 +45,12 @@ public class EndGame extends Fragment {
                 binding.star5
         };
 
+        binding.rbNone.setButtonDrawable(null);
+        binding.rbFail.setButtonDrawable(null);
+        binding.rbL1.setButtonDrawable(null);
+        binding.rbL2.setButtonDrawable(null);
+        binding.rbL3.setButtonDrawable(null);
+
         return binding.getRoot();
     }
 
@@ -56,19 +58,16 @@ public class EndGame extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Setup climb level toggle buttons
+        MatchData data = UserModel.getMatchData();
+
         setupClimbLevelButtons();
 
-        // Setup sliders
         setupSliders();
 
-        // Setup star rating
         setupStarRating();
 
-        // Reset button
         binding.tvReset.setOnClickListener(v -> resetAllFields());
 
-        // Match notes input
         binding.etMatchNotes.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
@@ -79,12 +78,10 @@ public class EndGame extends Fragment {
             @SuppressLint("SetTextI18n")
             @Override
             public void afterTextChanged(Editable s) {
-                // Save notes to your data model
-                // UserModel.getMatchData().setNotes(s.toString());
+                data.setNotes(s.toString());
             }
         });
 
-        // Submit button
         binding.btnSubmit.setOnClickListener(view1 -> UIHelpers.makeConfirmationAlert("Submit Match Data",
                 "Do you want to submit your match data?", () -> {
                 }, () -> {}, getContext()));
@@ -92,34 +89,32 @@ public class EndGame extends Fragment {
     }
 
     private void setupClimbLevelButtons() {
-        TextView[] buttons = {
-                binding.btnNone,
-                binding.btnFail,
-                binding.btnL1,
-                binding.btnL2,
-                binding.btnL3
-        };
+        MatchData data = UserModel.getMatchData();
 
+        ((RadioButton) binding.radioGroupClimbLevel.getChildAt(data.getTeleOpClimb())).setChecked(true);
 
-        for (int i = 0; i < buttons.length; i++) {
-            final int index = i;
-            buttons[i].setOnClickListener(v -> updateClimbButtons(buttons, index));
-        }
-
-        // Set initial selection (L3 is selected by default)
-        updateClimbButtons(buttons, 4);
+        binding.radioGroupClimbLevel.setOnCheckedChangeListener((r, i) -> {
+            int selectedIndex = binding.radioGroupClimbLevel.indexOfChild(binding.radioGroupClimbLevel.findViewById(i));
+            data.setTeleOpClimb(selectedIndex);
+            RadioButton[] buttons = {
+                    binding.rbNone,
+                    binding.rbFail,
+                    binding.rbL1,
+                    binding.rbL2,
+                    binding.rbL3
+            };
+            updateClimbButtons(buttons, selectedIndex);
+        });
     }
 
-    private void updateClimbButtons(TextView[] buttons, int selectedIndex) {
+    private void updateClimbButtons(RadioButton[] buttons, int selectedIndex) {
         for (int i = 0; i < buttons.length; i++) {
             if (i == selectedIndex) {
-                // Selected state
                 buttons[i].setBackgroundResource(R.drawable.cr6b0d59f2);
                 buttons[i].setTextColor(Color.WHITE);
                 buttons[i].setTypeface(null, android.graphics.Typeface.BOLD);
                 buttons[i].setElevation(4f);
             } else {
-                // Unselected state
                 buttons[i].setBackground(null);
                 buttons[i].setTextColor(Color.parseColor("#94A3B8"));
                 buttons[i].setTypeface(null, android.graphics.Typeface.NORMAL);
@@ -129,42 +124,48 @@ public class EndGame extends Fragment {
     }
 
     private void setupSliders() {
-        // Defense Duration Slider
-        binding.sliderDefense.addOnChangeListener((slider, value, fromUser) -> binding.tvDefenseValue.setText(String.valueOf((int) value)));
+        MatchData data = UserModel.getMatchData();
 
-        // Under Defense Slider
-        binding.sliderUnderDefense.addOnChangeListener((slider, value, fromUser) -> binding.tvUnderDefenseValue.setText(String.valueOf((int) value)));
+        binding.sliderDefense.addOnChangeListener((slider, value, fromUser) -> {
+            binding.tvDefenseValue.setText(String.valueOf((int) value));
+            data.setDefDuration((int) value);
+        });
 
-        // Broke Duration Slider
-        binding.sliderBroke.addOnChangeListener((slider, value, fromUser) -> binding.tvBrokeValue.setText(String.valueOf((int) value)));
+        binding.sliderUnderDefense.addOnChangeListener((slider, value, fromUser) -> {
+            binding.tvUnderDefenseValue.setText(String.valueOf((int) value));
+            data.setunderDefDuration((int) value);
+        });
+
+        binding.sliderBroke.addOnChangeListener((slider, value, fromUser) -> {
+            binding.tvBrokeValue.setText(String.valueOf((int) value));
+            data.setBrokeDuration((int) value);
+        });
     }
 
     private void setupStarRating() {
-        // Set initial rating
+        MatchData data = UserModel.getMatchData();
+
         updateStars(currentRating);
 
-        // Add click listeners to stars
         for (int i = 0; i < stars.length; i++) {
             final int rating = i + 1;
             stars[i].setOnClickListener(v -> {
                 currentRating = rating;
                 updateStars(rating);
                 updateQualityBadge(rating);
+                data.setDefEffectiveness(rating);
             });
         }
 
-        // Set initial badge
         updateQualityBadge(currentRating);
     }
 
     private void updateStars(int rating) {
         for (int i = 0; i < stars.length; i++) {
             if (i < rating) {
-                // Filled star
                 stars[i].setImageResource(android.R.drawable.btn_star_big_on);
                 stars[i].setColorFilter(Color.parseColor("#0D59F2"), PorterDuff.Mode.SRC_IN);
             } else {
-                // Empty star
                 stars[i].setImageResource(android.R.drawable.btn_star_big_off);
                 stars[i].setColorFilter(Color.parseColor("#4A5568"), PorterDuff.Mode.SRC_IN);
             }
@@ -197,28 +198,27 @@ public class EndGame extends Fragment {
     }
 
     private void resetAllFields() {
-        // Reset climb level to default
-        TextView[] buttons = {
-                binding.btnNone,
-                binding.btnFail,
-                binding.btnL1,
-                binding.btnL2,
-                binding.btnL3
-        };
-        updateClimbButtons(buttons, 4);
+        MatchData data = UserModel.getMatchData();
 
-        // Reset sliders
+        ((RadioButton) binding.radioGroupClimbLevel.getChildAt(4)).setChecked(true);
+        data.setTeleOpClimb(4);
+
         binding.sliderDefense.setValue(30);
-        binding.sliderUnderDefense.setValue(0);
-        binding.sliderBroke.setValue(0);
+        data.setDefDuration(30);
 
-        // Reset star rating
+        binding.sliderUnderDefense.setValue(0);
+        data.setunderDefDuration(0);
+
+        binding.sliderBroke.setValue(0);
+        data.setBrokeDuration(0);
+
         currentRating = 4;
         updateStars(currentRating);
         updateQualityBadge(currentRating);
+        data.setDefEffectiveness(4);
 
-        // Clear notes
         binding.etMatchNotes.setText("");
+        data.setNotes("");
     }
 
 
